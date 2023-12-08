f214396e053dad55beb747ad872025a9998dd1a0
webopenfather <2903868567@qq.com>
2023-12-08 12:37:35 +0800

    feat: v0.0.4

---
+++ src/extension.ts
@@ -0,0 +1,19 @@
+import * as vscode from "vscode";
+
+import printExt from "./exts/print";
+import printAllCommentExt from "./exts/printAllComment";
+import printAllUncommentExt from "./exts/printAllUncomment";
+import printAllRemoveExt from "./exts/printAllRemove";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log(222222);
+  console.log(222222);
+  console.log(222222);
+  console.log(222222);
+  context.subscriptions.push(printExt);
+  context.subscriptions.push(printAllCommentExt);
+  context.subscriptions.push(printAllUncommentExt);
+  context.subscriptions.push(printAllRemoveExt);
+}
+
+export function deactivate() {}
